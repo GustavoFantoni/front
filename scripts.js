8e2425a198af21ee821787249b1e1f9bcb3a4c16
Gustavo Fantoni <gustavo.rosario@unifebe.edu.br>
2024-04-02 13:33:41 -0300

    Responsividade

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let btnMenu = document.querySelector(".btn-menu");
+let itemsNavbar = document.querySelector(".ulNavbar");
+
+btnMenu.addEventListener("click", () => {
+    itemsNavbar.classList.toggle("actived");
+});
