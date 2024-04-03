f8162e1f8bf1646b033a90cf38a4cf658cc6fc6a
Gustavo Fantoni <gustavo.rosario@unifebe.edu.br>
2024-04-03 14:40:00 -0300

    Organização do código fonte

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
+// ACTIVATE MENU
 let btnMenu = document.querySelector(".btn-menu");
 let itemsNavbar = document.querySelector(".ulNavbar");
 
 btnMenu.addEventListener("click", () => {
     itemsNavbar.classList.toggle("actived");
 });
+// ACTIVATE MENU
