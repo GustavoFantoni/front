6b911b09046bad00bc865243be77b573ccb28074
Gustavo Fantoni <gustavo.rosario@unifebe.edu.br>
2024-04-10 13:40:46 -0300

    Atualização

--- scripts.js
+++ scripts.js
@@ -6,3 +6,26 @@ btnMenu.addEventListener("click", () => {
     itemsNavbar.classList.toggle("actived");
 });
 // ACTIVATE MENU
+
+
+
+
+// DESCRIPTION | PAGE PRODUCTS
+
+let cardArea = document.getElementsByClassName("card-prod");
+let description = document.getElementsByClassName("description-prod");
+
+for (let i = 0 ; i < cardArea.length ; i++) {
+    cardArea[i].addEventListener("mouseenter", () => {
+        description[i].style.color = "black";
+    })
+}
+for (let i = 0 ; i < cardArea.length ; i++) {
+    cardArea[i].addEventListener("mouseout", () => {
+        description[i].style.color = "transparent";
+    })
+}
+
+
+// DESCRIPTION | PAGE PRODUCTS
+
